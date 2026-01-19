04f6f8c92d304953ba8579771742952bf0662de8
Flossy-elephant <samyakahire2006@gmail.com>
2026-01-19 15:17:06 +0530

    just made this while I was learning how to use APIs

---
+++ script.mjs
@@ -0,0 +1,23 @@
+async function generate() {
+   try{
+        const response = await fetch('https://icanhazdadjoke.com/',
+            {
+                headers:{
+                    'Accept':'application/json'
+                }
+            }
+        )
+        const data = await response.json()
+
+        const jokeHolder = document.getElementById('mark')
+        jokeHolder.textContent = data.joke
+       
+    }
+    catch (err){
+        console.log(`something went wrong: ${err}`)
+    }
+}
+
+const button = document.getElementById('button')
+
+button.addEventListener('click',generate)
